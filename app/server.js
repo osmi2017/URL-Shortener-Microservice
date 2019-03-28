5dd88ace7a827509aa9c3cf9abf1796ba0aa787b
Soro Ismael <soroismael7@gmail.com>
2019-03-28 22:57:27 +0000

    Add files via upload

---
+++ app/server.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var express = require('express');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+var cors = require('cors');
+
+var bodyParser = require('body-parser');
+
+var urlHandler = require('./controllers/urlHandler.js');
+
+var app = express();
+
+// Basic Configuration 
+var port = process.env.PORT || 3000;
+
+/** this project needs a db !! **/ 
+mongoose.connect(process.env.MONGOLAB_URI);
+
+app.use(cors());
+
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+app.use(bodyParser.urlencoded({'extended': false}));
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/shorturl/new', urlHandler.addUrl);
+  
+app.get('/api/shorturl/:shurl', urlHandler.processShortUrl);
+  
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+app.listen(port, function () {
+  console.log('Node.js listening ...');
+});
